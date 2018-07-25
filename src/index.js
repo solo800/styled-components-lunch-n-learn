79374977e40101d9f0d0a383c395744b375230c0
Adam <adameliot1@gmail.com>
2018-07-25 11:27:46 -0700

    Updating to build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hello out there!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById("app"));
